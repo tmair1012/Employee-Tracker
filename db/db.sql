90792e4ba905385099ecf3288644dfdf9e47a6c8
TylerMair <tyleramair@gmail.com>
2021-10-24 22:26:36 -0700

    established database and connection

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS employeeData;
-Create DATABASE employeeData;
-USE employeeData;
+
+Create DATABASE employees;
+USE employees;
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const db = require('./db/connection');
+//const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// use apiRoutes
+//app.use('/api', apiRoutes);
+
+// Start server after DB
+db.connect(err => {
+    if (err) throw err;
+    console.log('Datavase connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    })
+})
